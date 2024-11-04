a846f42b9450e245f31260bb6a7ad81f36e700cb
ObsidianPresidium <emil@aarestrup.nu>
2024-11-04 17:20:44 +0100

    chore: add build, watch, start scripts

---
+++ web/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -d "./dist" ]; then
+    rm -r ./dist
+fi
+mkdir -p ./dist
+mkdir -p ./dist/css
+cp ./src/*.html ./dist/
+cp -r ./src/sample-data ./dist/
+npx tsc
+npx sass --no-source-map --style=compressed ./src/css/main.scss:./dist/css/main.css
