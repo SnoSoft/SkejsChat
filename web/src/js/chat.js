09975213c93d857b3d9b1b6828e35bd415d0fb1f
ObsidianPresidium <emil@aarestrup.nu>
2024-11-04 11:36:11 +0100

    refactor: rewrite chat.js to use a class

--- web/src/js/chat.js
+++ web/src/js/chat.js
@@ -1,44 +1,50 @@
-let messageData = {};
-let usernameData = {};
-let usernames = new Map();
+class SkejsChat {
+    messageData = {};
+    usernameData = {};
+    usernames = new Map();
 
-async function getMessages() {
-    const promise = await fetch("sample-data/message.json");
-    const messageData = await promise.json();
+    async getMessages() {
+        const promise = await fetch("sample-data/message.json");
+        const messageData = await promise.json();
 
-    return messageData;
+        return messageData;
+    };
+
+    async getUsernames() {
+        const promise = await fetch("sample-data/usernames.json");
+        const usernameData = await promise.json();
+    
+        return usernameData;
+    };
+
+    assignIdsToUsernames() {
+        let usernames = new Map();
+        this.usernameData.usernames.forEach((username) => {
+            usernames.set(username.id, username.name);
+        });
+
+        return usernames;
+    };
+    
+    async populate() {
+        this.messageData.messages.forEach((message) => {
+            const newNode = (message.Sender_id === 1) ? sc.createNode("message-by-user") : sc.createNode("message-not-by-user");
+            newNode.querySelector(".chat__message__user").textContent = `${this.usernames.get(message.Sender_id)}`;
+            newNode.querySelector(".chat__message__contents").textContent = message.Message;
+            document.querySelector(".chat__feed").appendChild(newNode);
+        });
+    };
+
+    async init() {
+        this.messageData = await this.getMessages();
+        this.usernameData = await this.getUsernames();
+        this.usernames = this.assignIdsToUsernames();
+        this.populate();
+    };
+
+    constructor() {
+        this.init();
+    };
 };
 
-async function getUsernames() {
-    const promise = await fetch("sample-data/usernames.json");
-    const usernameData = await promise.json();
-
-    return usernameData;
-};
-
-function assignIdsToUsernames() {
-    let usernames = new Map();
-    usernameData.usernames.forEach((username) => {
-        usernames.set(username.id, username.name);
-    });
-
-    return usernames;
-}
-
-async function populate() {
-    messageData.messages.forEach((message) => {
-        const newNode = (message.Sender_id === 1) ? sc.createNode("message-by-user") : sc.createNode("message-not-by-user");
-        newNode.querySelector(".chat__message__user").textContent = `${usernames.get(message.Sender_id)}`;
-        newNode.querySelector(".chat__message__contents").textContent = message.Message;
-        document.querySelector(".chat__feed").appendChild(newNode);
-    });
-};
-
-async function init() {
-    messageData = await getMessages();
-    usernameData = await getUsernames();
-    usernames = assignIdsToUsernames();
-    populate();
-};
-
-init();
+const schat = new SkejsChat();
