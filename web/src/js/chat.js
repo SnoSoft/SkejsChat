bb5d344a013382f1af3413f0c46579494e5c6e7e
ObsidianPresidium <emil@aarestrup.nu>
2024-11-04 11:19:21 +0100

    feat: map username id to actual name

--- web/src/js/chat.js
+++ web/src/js/chat.js
@@ -1,24 +1,44 @@
-let data = {};
+let messageData = {};
+let usernameData = {};
+let usernames = new Map();
 
 async function getMessages() {
     const promise = await fetch("sample-data/message.json");
-    const data = await promise.json();
+    const messageData = await promise.json();
 
-    return data;
+    return messageData;
+};
+
+async function getUsernames() {
+    const promise = await fetch("sample-data/usernames.json");
+    const usernameData = await promise.json();
+
+    return usernameData;
+};
+
+function assignIdsToUsernames() {
+    let usernames = new Map();
+    usernameData.usernames.forEach((username) => {
+        usernames.set(username.id, username.name);
+    });
+
+    return usernames;
 }
 
 async function populate() {
-    data.messages.forEach((message) => {
+    messageData.messages.forEach((message) => {
         const newNode = (message.Sender_id === 1) ? sc.createNode("message-by-user") : sc.createNode("message-not-by-user");
-        newNode.querySelector(".chat__message__user").innerHTML = `Sender: ${message.Sender_id}`;
-        newNode.querySelector(".chat__message__contents").innerHTML = message.Message;
+        newNode.querySelector(".chat__message__user").textContent = `${usernames.get(message.Sender_id)}`;
+        newNode.querySelector(".chat__message__contents").textContent = message.Message;
         document.querySelector(".chat__feed").appendChild(newNode);
     });
-}
+};
 
 async function init() {
-    data = await getMessages();
+    messageData = await getMessages();
+    usernameData = await getUsernames();
+    usernames = assignIdsToUsernames();
     populate();
-}
+};
 
 init();
